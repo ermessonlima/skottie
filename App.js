baf4b0a2aa9d5eeaafcccf83d9783b2651012ab5
Ermesson Lima <ermessonlima@MacBook-Air-de-Ermesson.local>
2024-04-03 13:20:26 -0300

    first

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import LottieAnimationFile from './test.json';
+import { Skottie } from 'react-native-skottie';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Skottie
+      style={{
+        width: 400,
+        height: 400,
+      }}
+      source={LottieAnimationFile}
+      autoPlay={true}
+    />
     </View>
   );
 }
